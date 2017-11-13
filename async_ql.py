486805c960d07a419953a153cf9fb10d3e89685a
alexTsaptsinos <atsaptsinos@gmail.com>
2017-11-13 00:18:31 -0800

    finished adding asynchronous

--- async_ql.py
+++ async_ql.py
@@ -29,9 +29,10 @@ EPSILON_START = 1.0
 EPSILON_MIN = 0.1
 EPSILON_FRAME = 1000000
 EPSILON_TRAINING_PERIOD = 50000
+PRED_UPDATE_RATE = 32
 TARGET_UPDATE_RATE = 10000
 ACTION_REPEAT = 4
-NUM_THREADS = 8
+NUM_THREADS = 2
 
 ################################################################################
 # Define hyperparameters
@@ -40,6 +41,30 @@ NUM_HIDDEN_UNITS = 100
 LR = 0.00025 # learning rate
 NUM_HIDDEN_UNITS = 10
 MOMENTUM = 0.95
+
+################################################################################
+# Define helper functions
+################################################################################
+def copy_vars(sess, from_scope, to_scope):
+    from_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, from_scope)
+    to_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, to_scope)
+
+    op_holder = [to_var.assign(from_var) for from_var, to_var in zip(from_vars, to_vars)]
+
+    def run_op():
+        sess.run(op_holder)
+
+    return run_op
+
+def increment_global_frame_op(sess, global_frame):
+    op = tf.assign(global_frame, global_frame+1)
+
+    def run_op():
+        _, step = sess.run([op, global_frame])
+        return step
+
+    return run_op
+
 
 ################################################################################
 # Define Environment wrapper
@@ -111,152 +136,197 @@ class EnvWrapper(object):
 
 
 ################################################################################
+# Define worker
+################################################################################
+class Worker(object):
+    
+    def __init__(self, sess, thread_id, coord, global_frame, env, pred_network, target_network):
+        self.sess = sess
+        # print sess._closed
+        self.thread_id = thread_id
+        self.coord = coord
+        self.env = env
+        self.pred_network = pred_network
+        self.target_network = target_network
+        self.target_update = copy_vars(sess, 'pred', 'target')
+        self.global_frame = global_frame
+        self.increment_global_frame = increment_global_frame_op(sess, global_frame)
+        self.global_frame_lock = threading.Lock()
+    
+    
+    def work(self):
+        """Trains the RL Agent"""
+
+        epsilon = EPSILON_START
+        episode = 0
+
+        print 'Starting worker {} with final epsilon {}'.format(self.thread_id,EPSILON_MIN)
+
+        while not self.coord.should_stop():
+
+            cur_state = self.env.start_state()
+            done = False
+
+            # Set up per-episode counters
+            ep_reward = 0
+            ep_ave_max_q = 0
+            step = 0
+            
+            state_batch = []
+            action_batch = []
+            target_batch = []
+
+            while not done:
+                with self.global_frame_lock:
+                    global_frame = self.increment_global_frame()
+
+                # Forward the deep q network, get Q(s,a) values
+                pred_qvals = self.pred_network.predict(self.sess, cur_state)
+
+                # Encode the action in a one hot vector
+                action = np.zeros([self.env.num_actions])
+                if np.random.random() < epsilon:
+                    action_idx = random.randrange(self.env.num_actions)
+                else:
+                    action_idx = np.argmax(pred_qvals)
+                action[action_idx]=1
+
+                target = 0
+                for i in range(ACTION_REPEAT):
+                    # Get new state and reward from environment
+                    new_state, reward, done, info = self.env.step(action_idx)
+                    ep_reward += reward
+                    ep_ave_max_q += np.max(pred_qvals)
+                    target += reward
+
+                if not done:
+                    target_qvals = self.target_network.predict(self.sess, new_state)
+                    target += DISCOUNT * np.max(target_qvals)
+
+                # Add to batch
+                state_batch.append(cur_state)
+                action_batch.append(action)
+                target_batch.append(target)
+
+                # Update pred network
+                if step % PRED_UPDATE_RATE == 0 or done:
+                    # In case we are done without adding anything new to batch
+                    # if state_batch:
+                    self.pred_network.update(self.sess, state_batch, action_batch, target_batch)
+                    state_batch, action_batch, target_batch = [], [] ,[]
+
+                # Update target network
+                if global_frame % TARGET_UPDATE_RATE == 0:
+                    print "Updating target network..."
+                    self.target_update()
+
+                # Update epsilon
+                if epsilon > EPSILON_MIN and global_frame > EPSILON_TRAINING_PERIOD:
+                    epsilon -= (EPSILON_START - EPSILON_MIN)/EPSILON_FRAME
+
+                # If max step is reached, request all threads to stop
+                if global_frame >= NUM_FRAMES:
+                    self.coord.request_stop()
+
+                cur_state = new_state
+                step+=1
+
+            # Episode finished, print stats
+            episode += 1
+            print "Worker: {}  Episode: {}  Step: {}  Frame: {}  Reward: {}  Qmax: {}  Epsilon: {}".\
+                format(self.thread_id, episode, step, global_frame, ep_reward, ep_ave_max_q/float(step), epsilon)
+
+
+
+################################################################################
 # Define network
 ################################################################################
-class QlWorker(object):
+class DQN(object):
 
-    def __init__(self, env, sess):
-        self.env = env
-        self.sess = sess
+    def __init__(self, num_actions, scope):
+        self.num_actions = num_actions
+        self.scope = scope
 
-        self.build_graph()
-        sess.run(tf.global_variables_initializer())
+        self.build_dqn()
+        if scope == 'pred':
+            self.build_updater()
 
-    def build_qn(self):
-        inputs = tf.placeholder(tf.float32, [None] + list(INPUT_SHAPE))
-        net = tflearn.flatten(inputs)
-        net = tflearn.fully_connected(net,NUM_HIDDEN_UNITS,activation='relu')
-        qvals = tflearn.fully_connected(net, self.env.num_actions)
-        return inputs, qvals
+    def build_dqn(self):
+        '''Defines the value function model'''
 
-    def build_graph(self):
-        # Define prediction model
-        self.pred_inputs, self.pred_qvals = self.build_qn()
-        network_params = tf.trainable_variables()
-
-        # Define target model
-        self.target_inputs, self.target_qvals = self.build_qn()
-        target_network_params = tf.trainable_variables()[len(network_params):]
-
-        # Define model updater (SGD)
-        self.actions = tf.placeholder(tf.float32, [None, self.env.num_actions])
-        self.targets = tf.placeholder(tf.float32, [None])
-        Qopts = tf.reduce_sum(tf.multiply(self.pred_qvals, self.actions), reduction_indices=1)
-        cost = tflearn.mean_square(Qopts, self.targets)
-        optimizer = tf.train.RMSPropOptimizer(LR, decay = MOMENTUM) # change different optimizer
-        self.updateModel = optimizer.minimize(cost, var_list=network_params)
-
-        # Define updating target model with prediction model
-        self.update_target_network_params = \
-                [target_network_params[i].assign(network_params[i])
-                        for i in range(len(target_network_params))]
-
+        with tf.variable_scope(self.scope):
+            self.inputs = tf.placeholder(tf.float32, [None] + list(INPUT_SHAPE))
+            net = tflearn.flatten(self.inputs)
+            net = tflearn.fully_connected(net,NUM_HIDDEN_UNITS,activation='relu')
+            self.qvals = tflearn.fully_connected(net, self.num_actions)
         return
 
-    def pred_qvals(self, state, is_target):
-        if is_target:
-            return self.target_qvals.eval(session=self.sess, feed_dict={self.inputs: [state]})
-        else:
-            return self.qvals.eval(session=self.sess, feed_dict={self.inputs: [state]})
+    def build_updater(self):
+        '''Defines the model cost, optimizer and updater'''
 
-    def update_model(self, action, target):
-        self.sess.run(self.updateModel, feed_dict={self.actions: [action], self.targets: [target]})
+        # Define model updater (SGD)
+        self.actions = tf.placeholder(tf.float32, [None, self.num_actions])
+        self.targets = tf.placeholder(tf.float32, [None])
+        Qopts = tf.reduce_sum(tf.multiply(self.qvals, self.actions), reduction_indices=1)
+        cost = tflearn.mean_square(Qopts, self.targets)
+        optimizer = tf.train.RMSPropOptimizer(LR, decay = MOMENTUM) # change different optimizer
+        local_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, self.scope)
+        self.updateModel = optimizer.minimize(cost, var_list=local_vars)
 
+        return
+    
+    def predict(self, sess, state):
+        return self.qvals.eval(session=sess, feed_dict={self.inputs: [state]})
 
-
+    def update(self, sess, states, actions, targets):
+        sess.run(self.updateModel, feed_dict={self.inputs: states, self.actions: actions, self.targets: targets})
 
 
 ################################################################################
 # Train agent
 ################################################################################
-def train_thread(sess, thread_id, env):
-    """Trains the RL Agent"""
-
-    #env = EnvWrapper(gym.make(GAME), BUFFER_SIZE)
-    dqn_agent = QlWorker(env, sess)
-    epsilon = EPSILON_START
-    frame = 0
-    episode = 0
-
-    while frame < NUM_FRAMES:
-        cur_state = env.start_state()
-        done = False
-
-        # Set up per-episode counters
-        ep_reward = 0
-        ep_ave_max_q = 0
-        step = 0
-
-        while not done:
-            # Forward the deep q network, get Q(s,a) values
-            # pred_qvals = dqn_agent.pred_qvals(cur_state, False)
-            pred_qvals = sess.run(dqn_agent.pred_qvals, feed_dict={dqn_agent.pred_inputs: [cur_state]})
-            # Encode the action in a one hot vector
-            action = np.zeros([env.num_actions])
-            if np.random.random() < epsilon:
-                action_idx = random.randrange(env.num_actions)
-                # Qopt = pred_qvals[action_idx]
-            else:
-                action_idx = np.argmax(pred_qvals)
-                # Qopt = np.max(pred_qvals)
-            action[action_idx]=1
-
-            target = 0
-            for i in range(ACTION_REPEAT):
-                # Get new state and reward from environment
-                new_state, reward, done, info = env.step(action_idx)
-                ep_reward += reward
-                ep_ave_max_q += np.max(pred_qvals)
-                target += reward
-
-            if not done:
-                # target_state = dqn_agent.pred_qvals(new_state, True)
-                target_qvals = sess.run(dqn_agent.target_qvals, \
-                        feed_dict={dqn_agent.target_inputs: [new_state]})
-                target += DISCOUNT * np.max(target_qvals)
-
-            # Update network weights
-            sess.run(dqn_agent.updateModel, feed_dict={dqn_agent.actions: [action], dqn_agent.targets: [target], dqn_agent.pred_inputs: [cur_state]})
-            # dqn_agent.update_model(action, target)
-
-            # Update target network
-            if frame % TARGET_UPDATE_RATE == 0:
-                sess.run(dqn_agent.update_target_network_params)
-
-            if epsilon > EPSILON_MIN and frame > EPSILON_TRAINING_PERIOD:
-                epsilon -= (EPSILON_START - EPSILON_MIN)/EPSILON_FRAME
-
-            cur_state = new_state
-            step+=1
-
-        # Episode finished, print stats
-        episode += 1
-        frame += step
-        print "Episode: {}  Step: {}  Frame: {}  Reward: {}  Qmax: {}  Epsilon: {}".\
-            format(episode, step, frame, ep_reward, ep_ave_max_q/float(step), epsilon)
-
-def test(thread_id, env):
-    time.sleep(3*thread_id)
-    print 'hi from threadID {}'.format(thread_id)
-    tempList = []
-    tempList.append(thread_id)
-
-    if tempList:
-        print tempList
-    print 'not tempList: {}'.format(tempList)
 
 def train(sess):
+    '''Launches the training by creating parallel threads, launching agents in each thread and starting each agent learning'''
 
+    # Create global step counter
+    global_frame = tf.Variable(name='global_frame', initial_value=0, trainable=False, dtype=tf.int32)
+
+    # Get num actions
+    env = gym.make(GAME)
+    num_actions = env.action_space.n
+    env.close()
+
+    # Create shared networks
+    pred_network = DQN(num_actions, 'pred')
+    target_network = DQN(num_actions, 'target')
+
+    # Init variables
+    sess.run(tf.global_variables_initializer())
+    first_update = copy_vars(sess, 'pred','target')
+    first_update()
+
+    # Create thread coordinator
+    coord = tf.train.Coordinator()
+
+    # Create environment for each thread
     env = [EnvWrapper(gym.make(GAME), BUFFER_SIZE) for i in range(NUM_THREADS)]
 
-    #threads = [threading.Thread(target=train_thread, args(sess, thread_id, env[thread_id]) \
-            #for thread_id  in range(NUM_THREADS)]
+    # Create workers for each thread
+    workers = []
+    threads = []
+    for thread_id in range(NUM_THREADS):
+        worker = Worker(sess, thread_id, coord, global_frame, env[thread_id], pred_network, target_network)
+        workers.append(worker)
+        worker_work = lambda: worker.work()
+        t = threading.Thread(target=worker_work)
+        threads.append(t)
+        # t.daemon=True # WHAT
+        t.start()
+        time.sleep(0.01) # needed?
 
-    threads = [threading.Thread(target=test, args = (thread_id, env[thread_id])) \
-            for thread_id  in range(NUM_THREADS)]
+    coord.join(threads)
 
-    for thread in threads:
-        thread.start()
 
 if __name__ == "__main__":
     with tf.Session() as sess:
